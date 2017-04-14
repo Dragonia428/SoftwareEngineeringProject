c05c87caf264f39d07f782a411ca0ff11656b1e2
gurbos <galvin.burgos@gmail.com>
2017-04-13 23:01:56 -0400

    Added pending_accounts table for accounts awaiting approval from restaurant manager

--- Connect.java
+++ Connect.java
@@ -31,7 +31,6 @@ public class Connect {
 			System.out.println("ERROR OCCURED");
 			ex.printStackTrace();
 		}
-		
 
 	}
 	private String ChefsTable()
@@ -44,7 +43,7 @@ public class Connect {
 	{
 		StringBuilder str = new StringBuilder();
 		str.append("CREATE TABLE users(id int(11) NOT NULL AUTO_INCREMENT,");
-		str.append("first_name varchar(20), last_name varchar(20), email varchar(26), password varchar(10), status varchar(3), PRIMARY KEY(id));");
+		str.append("first_name varchar(20), last_name varchar(20), email varchar(30), password varchar(10), status varchar(3), PRIMARY KEY(id));");
 		return str.toString();	
 	}
 	private String DishesTable()
@@ -53,6 +52,11 @@ public class Connect {
 		str.append("CREATE TABLE dishes(chef_name varchar(20), dish_name varchar(20), price int(5), type varchar(10), FOREIGN KEY(chef_name) REFERENCES chefs(chef_name));");
 		return str.toString();
 	}
+    private String PendingAccounts() {
+        StringBuilder str = new StringBuilder();
+        str.append("CREATE TABLE pending_accounts(email varchar(30), first_name varchar(20), last_name varchar(20), password varchar(20), PRIMARY KEY(email));");
+        return str.toString();
+    }
 	private void CreateDatabase(String DBName)
 	{
 
