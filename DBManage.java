168071977a5fce0d78254482a7da762c260d70bd
Dragonia428 <settineri.john@gmail.com>
2017-04-16 14:02:55 -0400

    Adding Login page + DBManage start

--- DBManage.java
+++ DBManage.java
@@ -1,4 +1,14 @@
+import java.sql.*;
 public class DBManage
 {
-	//public 
+	private String Users()
+	{
+		StringBuilder str = new StringBuilder();
+		str.append("INSERT INTO users(first_name, last_name, email, password, status) VALUES(?,?,?,?,?)");
+		return str.toString();
+	}
+	public void AddToUsersTable()
+	{
+
+	}
 }
