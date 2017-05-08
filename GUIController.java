82bb291870dee2754b26cb2e9eb4827005af59b0
Dragonia428 <settineri.john@gmail.com>
2017-05-08 18:57:59 -0400

    Pushing GUIController up. Integrated GUI controller with DB

---
+++ GUIController.java
@@ -0,0 +1,106 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent; 
+import javafx.stage.Stage;
+import javafx.scene.Scene; 
+import java.io.IOException;
+import javafx.scene.layout.AnchorPane; 
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+public class GUIController implements Initializable {
+    private Parent root;
+    public Stage curr_stage = new Stage();
+    /* 
+    For the login page 
+    These represent the values for the password and the username
+    */
+    @FXML PasswordField passwordfield;
+    @FXML TextField textfield;
+    /* For the registration page 
+    These represent the passwordfield, reg*/
+    @FXML PasswordField regpasswordfield;
+    @FXML PasswordField verifypasswordfield; 
+     @FXML TextField regemailfield; 
+     @FXML TextField firstnamefield;
+     @FXML TextField lastnamefield; 
+     @FXML Label notificationmessage; 
+     /* End the registration stuff */
+
+     /*DB Manager -> Manages the database */
+     DBManage dbmanager = new DBManage(Connect.databaselink);
+
+    @Override 
+    public void initialize(URL url, ResourceBundle rb)
+    {
+
+    }
+    public void GoToLogin(Stage primaryStage) throws IOException
+    {
+        try{
+        
+        curr_stage = primaryStage;
+        curr_stage.setTitle("Welcome");
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root);
+      //  textfield = new TextField();
+        curr_stage.setScene(scene);
+        curr_stage.show();
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    @FXML 
+    private void OnLogin()
+    {
+        System.out.println(textfield.getText());
+    }
+    @FXML private void GoToRegistration() throws IOException
+    {   
+        try {
+            
+            Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+            Scene scene = new Scene(root);
+            curr_stage.setScene(scene);
+            curr_stage.show();
+         }
+         catch(IOException ex)
+         {
+            System.out.println("ERROR");
+         }
+      
+    }
+    @FXML private void Register(ActionEvent e)
+    {
+        String email = regemailfield.getText();
+        String password = regpasswordfield.getText();
+        String firstName = firstnamefield.getText();
+        String lastName = lastnamefield.getText();
+        String verify = verifypasswordfield.getText();
+        System.out.println(PasswordStrength.CheckPassword(password));
+        if(!password.equals(verify))
+        {
+            notificationmessage.setText("Passwords are not the same.");
+        }
+        else {
+            if(PasswordStrength.CheckPassword(password) == 5 || PasswordStrength.CheckPassword(password) == 4)
+            {
+                notificationmessage.setText("");
+                System.out.println("Adding to pending accounts");
+                dbmanager.addToPendingAccountsTable(email, firstName, lastName, password);
+  
+            }
+            else {
+                notificationmessage.setText("Password is not very strong");
+            }
+        }
+    }
+}
