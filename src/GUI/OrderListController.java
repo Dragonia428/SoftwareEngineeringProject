c06675560620dfc06b6d63056e76eb4a2972c5ae
Miguel Angel Rodriguez <Marodriguez148@gmail.com>
2017-05-15 19:24:22 -0400

    Updated Orderfood function

--- src/GUI/OrderListController.java
+++ src/GUI/OrderListController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import java.sql.*;
 import javafx.scene.control.*;
+import java.util.Random;
 /**
  * FXML Controller class
  *
@@ -21,20 +22,50 @@ public class OrderListController implements Initializable {
     @FXML Label total_price;
     @FXML TableColumn pricecol;
     @FXML TableColumn foodcol;
-    @FXML TableView cart; 
+    @FXML TableView cart;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }  
+    }
     @FXML private void OrderFood()
     {
-        double price = Float.parseFloat(total_price.getText());
-        UpdateCustomerTable(UserInfo.email, price);
-        
+      try{
+        DBManage dbmanage = new DBManage();
+        ArrayList<int> deliveryarray, disharray;
+        int customer_id, dptoDeliver;
+        Random rand;
+        ResultSet customerid_rs = dbmanage.queryDatabase("select customer_id from customers where email="+userinfo.email+";");
+        ResultSet delivery_rs = dbmanage.queryDatabase("select delivery_id from delivery;");
+        ResultSet shopping_rs = dbmanage.queryDatabase("select dish_name from shopping_cart;");
+        customrid_rs.next();
+        customer_id = customerid_rs.getInt("customer_id");
+        java.sql.Date date = new java.sql.Date(currentDate.getTime());
+        total_price = 0;
+
+        while(delivery_rs.next()){
+          deliveryarray.add(delivery_rs.getInt);
+        }
+
+        dptoDeliver = deliveryarray.get(rand.getInt());
+
+        while(shopping_rs.next()){
+          ResultSet dish_rs = dbmanage.queryDatabase("select dish_id from dishes WHERE dish_name ="+shopping_rs.getString()+";");
+          dish_rs.next();
+          disharray.add(dish_rs.getInt());
+        }
+
+        for(int i = 0; i < disharray.size; i++){
+          dbmanage.addtoOrdersTable(customer_id, date, disharray.get(i), dptoDeliver);
+        }
+      }
+      catch(SQLException sqlException){
+        sqlException.printStackTrace();
+      }
     }
+    
     private void UpdateCustomerTable(String email, double total_price)
     {
         java.sql.Date date = new java.sql.Date(System.currentTimeMillis());
@@ -42,12 +73,12 @@ public class OrderListController implements Initializable {
             Connection con = DriverManager.getConnection(Connect.databaselink, "root", "123456");
             Statement st = con.createStatement();
             st.executeUpdate("update customers set funds = funds - " + total_price + " where email =" + "'" + email + "'");
-            
+
         }
         catch(SQLException ex)
         {
-            
+
         }
     }
-    
+
 }
